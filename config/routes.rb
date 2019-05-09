a9b6f317e2a6cd1ff87a86cdbdaaadad19993ca5
AMargaillan <adeline.margaillan@gmail.com>
2019-05-09 04:17:40 +0000

    pagination

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  root 'places#index'
+  resources :places, only: [:index]
+  root to: 'places#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
 end
