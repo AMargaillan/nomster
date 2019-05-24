c406acb29aa62d5fb8caa811d0c6d9eaadc4b103
AMargaillan <adeline.margaillan@gmail.com>
2019-05-24 03:30:35 +0000

    size

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -10,7 +10,15 @@ class Comment < ApplicationRecord
     'Five Stars': '5_stars'
   }
 
+puts "we are hitting the comments file"
+
   def humanized_rating
+    puts "_______"
+    puts RATINGS.invert
+    puts "RATINGS"
+    puts RATINGS
+    puts "RATINS INVERT slef.rating"
+    puts RATINGS.invert[rating]
     RATINGS.invert[self.rating]
   end
 end
