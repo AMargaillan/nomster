82a572e11a2b197916e12f1d3caaa77f9dd0b3c6
AMargaillan <adeline.margaillan@gmail.com>
2019-05-29 00:41:49 +0000

    error

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,14 +2,14 @@ class PhotosController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    @photo = Photo.new(photo_params)
     @place = Place.find(params[:place_id])
+    @place.photos.create(photos_params.merge(user: current_user))
     redirect_to place_path(@place)
   end
 
   private
 
   def photo_params
-    params.require(:photo).permit(:picture, :place_id, :user_id)
+    params.require(:photo).permit(:picture)
   end
 end
